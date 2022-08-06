47402d5257acb7dad50384e0d837cf61a70fdaf3
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-08-05 19:38:29 -0600

    set up lengthChoice variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,14 @@
 // arrays of valid characters
-var lowerCase = [];
-var upperCase = [];
+var lowerCaseChar = [];
+var upperCaseChar = [];
+var numericChar = [];
+var specialChar = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  console.log("hi");
   var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
@@ -17,19 +18,41 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// function getUserChoices() {
-//   var lengthChoice = parseInt(
-//     prompt("How many characters do you want your password to contain?")
-//   );
-//   if (Number.isNaN(lengthChoice)) {
-//     alert("Password length must be a number.");
-//   }
-//   if (length < 8) {
-//     alert("Password must be at least 8 characters.");
-//   }
-//   if (length > 128) {
-//     alert("Password must be at least 8 characters.");
-//   }
-// }
+function getUserChoice() {
+  var lengthChoice = parseInt(
+    prompt(
+      "How many characters do you want your password to be?" +
+        "\n" +
+        "Password must be at least 8 characters and no more than 128 characters."
+    )
+  );
+  if (Number.isNaN(lengthChoice)) {
+    alert("Password length must be a number.");
+  } else if (lengthChoice < 8) {
+    alert("Password must be at least 8 characters.");
+  } else if (lengthChoice > 128) {
+    alert("Password must be no more than 128 characters.");
+  } else alert("cool");
+}
 
-// write generatePasssword() function
+getUserChoice();
+
+//
+//
+// write getUserChoices() function
+//    -prompt user to enter length
+//        -input has to be a number
+//        -input has to be at least 8 and no more than 128
+//    -prompt user to confirm lowercase
+//    -prompt user to confirm uppercase
+//    -prompt user to confirm numeric
+//    -prompt user to confirm special characters
+//        -at least one character type must be confirmed
+// write generatePassword() function
+//    -generate password based on user inputs
+
+//
+//
+// call getUserChoices() at top of writePassword(), above generatePassword()
+
+//
