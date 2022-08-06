1e4ed3fb3bbb01db9afe13e9c3a8af9ac8bcb45e
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-08-06 10:43:56 -0600

    updates to prompt flow

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// arrays of valid characters
+// arrays of possible characters
 var lowerChar = [
   "a",
   "b",
@@ -103,15 +103,15 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+// when button is clicked, run getChoices
 function writePassword() {
-  getChoices();
-
+  getChoices(); // gets user input and generates random password
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = password; // assigns value of passwordText on page to password variable determined in getChoices() function
 }
 
 function getChoices() {
+  // stores the user input as a datatype of integer instead of a string
   var lengthChoice = parseInt(
     prompt(
       "How many characters do you want your password to be?" +
@@ -119,45 +119,63 @@ function getChoices() {
         "Password must be at least 8 characters and no more than 128 characters."
     )
   );
+  // alerts the user if their entry is invalid and stops the function
   if (Number.isNaN(lengthChoice)) {
     alert("Password length must be a number.");
-    getChoices();
+    password = ""; // keeps the password box on the page blank
+    return;
   } else if (lengthChoice < 8) {
     alert("Password must be at least 8 characters.");
-    getChoices();
+    password = "";
+    return;
   } else if (lengthChoice > 128) {
     alert("Password must be no more than 128 characters.");
-    getChoices();
+    password = "";
+    return;
   } else
     var lowerChoice = confirm(
-      "Do you want your password to include lowercase letters?"
+      "Do you want your password to include lowercase letters?" +
+        "\n" +
+        "Click 'OK' for yes or 'Cancel' for no."
     );
+  // after asking if the user wants lowercase letters, moves on to the next question regardless of whether they say yes or no
   if (lowerChoice === true || lowerChoice === false) {
     var upperChoice = confirm(
-      "Do you want your password to include uppercase letters?"
+      "Do you want your password to include uppercase letters?" +
+        "\n" +
+        "Click 'OK' for yes or 'Cancel' for no."
     );
   } else if (upperChoice === true || upperChoice === false);
   {
     var numericChoice = confirm(
-      "Do you want your password to include numeric characters?"
+      "Do you want your password to include numeric characters?" +
+        "\n" +
+        "Click 'OK' for yes or 'Cancel' for no."
     );
     if (numericChoice === true || numericChoice === false) {
       var specialChoice = confirm(
-        "Do you want your password to include special characters?"
+        "Do you want your password to include special characters?" +
+          "\n" +
+          "Click 'OK' for yes or 'Cancel' for no."
       );
     }
+    // alerts the user if they said no to all four choices
     if (lowerChoice || upperChoice || numericChoice || specialChoice) {
       generatePassword();
     } else {
       var invalidInput = alert(
         "Password must include at least one of the following: lowercase letters, uppercase letters, numbers, special characters."
       );
+      // returns the user to the start of getChoices
       if (invalidInput || !invalidInput) {
-        getChoices();
+        password = "";
+        return;
       }
     }
   }
   function generatePassword() {
+    // creates a pool of characters which starts off empty
+    // adds character arrays based on user choices
     var pool = [];
     if (lowerChoice) {
       pool = pool.concat(lowerChar);
@@ -171,21 +189,14 @@ function getChoices() {
     if (specialChoice) {
       pool = pool.concat(specialChar);
     }
-    // we now have a pool of characters that the user wants included
-    // we use the random() function to randomly shuffle the pool
+    // uses the random() function to randomly shuffle the pool
     random(pool);
+    // slices off a new pool (from the now-randomized pool of confirmed characters) of the length entered by the user in lengthChoice
     slicedPool = pool.slice(0, lengthChoice);
+    // assigns the randomized, sliced pool as the value of the password variable
     password = slicedPool;
   }
 }
-//
-//
-//
-//
-// slice off string with number of characters determined by lengthChoice variable
-//
-// assign new string as value of password variable
-//
-//
-//
-// generatePassword() ultimately needs to assign a value to the password variable (last line of writePassword() function)
+
+// TODO: arrays need to be repeating?
+// currently if a user chooses a length of 100 but only says yes to numeric values, their password is a maximum 10 characters, 26 for letters, etc.
